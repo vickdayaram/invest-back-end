7a7cc74faa2f5943f340a30e2039004d05e9b34c
github username <github email address>
2017-08-09 12:51:03 -0400

    got back end response when posting a new user, getting back username and jwt as expected

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,9 +6,16 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    render json: @user
-  end 
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      created_jwt = issue_token({id: @user.id})
+
+      render json: {username: @user.username, jwt: created_jwt}
+    else
+      render json: {errors: @user.errors.full_messages}
+    end
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/users', to: 'users#index'
       post '/users', to: 'users#create'
     end
   end

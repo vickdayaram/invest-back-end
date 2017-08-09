06e9d2374cca824160374f6ed516f09d3748aac7
github username <github email address>
2017-08-09 13:28:27 -0400

    set up auth, login, and signup, functioning at this point

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      post '/users', to: 'users#create'
+      post '/signup', to: 'users#create'
+      post '/login', to: 'auth#create'
     end
   end
 end
